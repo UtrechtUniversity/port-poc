36dbfe991e60862a7cbb2027ddc3ec4bc3146328
Haili Hu <hailihu@gmail.com>
2021-05-03 10:20:14 +0200

    cleanup

--- data_extractor/google_semantic_location_history/__init__.py
+++ data_extractor/google_semantic_location_history/__init__.py
@@ -11,31 +11,31 @@ import pandas as pd
 def process(file_data):
     """Return relevant data from zipfile for years and months
     Args:
-        file_data: zipfile
-        years: list with years
-        months: list with months
+        file_data: zip file or object
 
     Returns:
-        pd.DataFrame: dataframe with relevant info
+        pd.DataFrame: DataFrame with relevant info
     """
     years = [2020, 2021]
     months = ["JANUARY"]
     results = []
-    names = []
+    filenames = []
+
+    # Extract info from selected years and months
     with zipfile.ZipFile(file_data) as zfile:
         for year in years:
             for month in months:
                 for name in zfile.namelist():
                     monthfile = f"{year}_{month}.json"
                     if re.search(monthfile, name) is not None:
-                        names.append(name)
+                        filenames.append(monthfile)
                         break
                 data = json.loads(zfile.read(name).decode("utf8"))
                 placevisit_duration = []
                 activity_duration = 0.0
                 for data_unit in data["timelineObjects"]:
                     if "placeVisit" in data_unit.keys():
-                        address = data_unit["placeVisit"]["location"]["address"].split("\n")[0]
+                        address = data_unit["placeVisit"]["location"]["placeId"]
                         start_time = data_unit["placeVisit"]["duration"]["startTimestampMs"]
                         end_time = data_unit["placeVisit"]["duration"]["endTimestampMs"]
                         placevisit_duration.append(
@@ -45,33 +45,46 @@ def process(file_data):
                         end_time = data_unit["activitySegment"]["duration"]["endTimestampMs"]
                         activity_duration += (int(end_time) - int(start_time))/(1e3*24*60*60)
 
-                address_list = [list(duration.keys())[0] for duration in placevisit_duration]
-                place_duration = sum(
+                # total time spend visiting places
+                place_duration_total = sum(
                     [list(duration.values())[0] for duration in placevisit_duration])
 
-                locations = {}
-                for address in set(address_list):
-                    loc_duration = sum(
-                        [duration[address] for duration in placevisit_duration \
-                            if address == list(duration.keys())[0]])
-                    locations[address] = loc_duration
+                # list of places visited
+                address_list = set([list(duration.keys())[0] for duration in placevisit_duration])
 
+                # dict of time spend per place
+                places = {}
+                for address in address_list:
+                    places[address] = round(sum(
+                        [duration[address] for duration in placevisit_duration \
+                            if address == list(duration.keys())[0]]), 3)
+
+                # Sort places to amount of time spend
                 sorted_places = collections.OrderedDict(
-                    sorted(locations.items(), key=lambda kv: kv[1], reverse=True))
-                top_locations = dict(itertools.islice(sorted_places.items(),3))
-                number = len(set(address_list))
+                    sorted(places.items(), key=lambda kv: kv[1], reverse=True))
 
                 results.append({
                     "Year": year,
                     "Month": month,
-                    "Locations": top_locations,
-                    "Number of Locations": number,
-                    "Place Duration": place_duration,
-                    "Acitivity Duration": activity_duration})
+                    "Top Places": dict(itertools.islice(sorted_places.items(), 3)),
+                    "Number of Places": len(address_list),
+                    "Places Duration": round(place_duration_total, 3),
+                    "Activity Duration": round(activity_duration, 3)
+                })
+
+    # Put results in DataFrame
+    df = pd.json_normalize(results)
+
+    # Anonymize by replace PlaceIds with numbers
+    number = 0
+    for column in df.columns:
+        if column.split(".")[0] == "Top Places":
+            number += 1
+            df.rename(columns = {column: f"Place {number}"}, inplace = True )
 
     return {
-        "summary": f"The following files where read: {', '.join(names)}.",
-        "data": pd.json_normalize(results)
+        "summary": f"The following files where read: {', '.join(filenames)}.",
+        "data": df
     }
 
 if __name__ == '__main__':
--- data_extractor/tests/test_google_semantic_location_history.py
+++ data_extractor/tests/test_google_semantic_location_history.py
@@ -5,5 +5,5 @@ DATA_PATH = Path(__file__).parent / "data"
 
 def test_gslh():
     result = process(DATA_PATH.joinpath("takeout-test.zip").open("rb"))
-    assert result["summary"] == 'The following files where read: Takeout/Location History/Semantic Location History/2020/2020_JANUARY.json, Takeout/Location History/Semantic Location History/2021/2021_JANUARY.json.'
+    assert result["summary"] == 'The following files where read: 2020_JANUARY.json, 2021_JANUARY.json.'
 
