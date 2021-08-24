79870241f954c04bb06a53c324f3bcfddabdbd18
Haili Hu <hailihu@gmail.com>
2021-08-24 13:15:03 +0200

    flake8

--- data_extractor/google_semantic_location_history/main.py
+++ data_extractor/google_semantic_location_history/main.py
@@ -5,4 +5,4 @@ from google_semantic_location_history import process
 if __name__ == '__main__':
     result = process("tests/data/Location History.zip")
     print("Summary:\n", result["summary"])
-    print("Dataframe\n" , result["data_frames"])
+    print("Dataframe\n", result["data_frames"])
