00f25c07400d5a62f1ec374e8877c77b0a3aa923
Roos Voorvaart <r.voorvaart@uu.nl>
2021-09-08 11:45:33 +0200

    add comments and remove print statements

--- data_extractor/tests/test_simulation_gsh.py
+++ data_extractor/tests/test_simulation_gsh.py
@@ -1,4 +1,4 @@
-"""Test data extraction from simulated Google Browser History .json file"""
+"""Test simulated Google BrowserHistory.json file"""
 
 import random
 
@@ -13,7 +13,6 @@ from google_search_history.simulation_gsh import __create_website, __create_date
 import pandas as pd
 
 SEED = 123
-
 PERIODS = {'before': ["20-10-2020 13:30:00", "23-01-2021 20:59:59"],
            'during': ["23-01-2021 21:00:00", "28-04-2021 04:29:59"]}
 
@@ -22,7 +21,6 @@ URLS_PRE = ['https://macias.com/', 'https://davis.info/', 'https://schaefer.org/
             'https://johnson-simmons.com/', 'https://nieuws.nl/',
             'http://www.becker.com/', 'http://adams.com/',
             'https://telegraaf.nl/']
-
 URLS = ['https://johnson-simmons.com/', 'https://davis.info/', 'https://macias.com/',
         'https://nos.nl/', 'https://johnston.com/', 'https://nieuws.nl/',
         'https://schaefer.org/', 'https://telegraaf.nl/', 'http://www.becker.com/',
@@ -32,7 +30,6 @@ DATES_PRE = [1603514795402199, 1603631814184174, 1603918582202212,
              1604087511327018, 1604515419677343, 1605936247715030,
              1606554235516584, 1607616223263477, 1610217532454797,
              1610621971858733]
-
 DATES = [1611746498925448, 1611864413000000, 1612736624846266,
          1613433337849162, 1614142545833878, 1614155572967477,
          1614183443314904, 1615804895407218, 1618378912170921,
@@ -57,11 +54,17 @@ EXPECTED = [
 
 
 def __set_seed():
+    """
+    sets seed for simulation procedure
+    """
     random.seed(SEED)
     Faker.seed(str(SEED))
 
 
 def __create_browserfile():
+    """
+    returns: simulated BrowserFile.json (as string)
+    """
     __set_seed()
     browserfile = browserhistory(
         num=20, site_diff=0.15, time_diff=0.2, seed=0, fake=True)
@@ -70,7 +73,7 @@ def __create_browserfile():
 
 def __reshape_expected():
     """
-    returns: excpeted outcome to sorted dataframe
+    returns: excpeted outcome (EXPECTED) as sorted pd.DataFrame
     """
     df_expected = pd.melt(pd.DataFrame(EXPECTED),
                           ["Curfew", "Website"],
@@ -82,18 +85,27 @@ def __reshape_expected():
 
 
 def test_create_website_pre():
+    """
+    simulates URLs with 15% news vs. 85% other websites
+    """
     __set_seed()
     urls_pre = __create_website(num=10, perc=0.15, fake=True)
     assert urls_pre == URLS_PRE
 
 
 def test_create_website_during():
+    """
+    simulates URLs with 30% news vs. 70% other websites
+    """
     __set_seed()
     urls = __create_website(num=10, perc=0.30, fake=True)
     assert urls == URLS
 
 
 def test_create_date_pre():
+    """
+    generates random dates between 20-10-2020 13:30:00 and 23-01-2021 20:59:59
+    """
     __set_seed()
     dates_pre = __create_date(num=10, start=PERIODS['before'][0],
                               end=PERIODS['before'][1], time_perc=0)
@@ -101,6 +113,10 @@ def test_create_date_pre():
 
 
 def test_create_date_during():
+    """
+    generates random dates between 23-01-2021 21:00:00 and 28-04-2021 04:29:59,
+    with at least 20% evening times.
+    """
     __set_seed()
     dates = __create_date(num=10, start=PERIODS['during'][0],
                           end=PERIODS['during'][1], time_perc=0.2)
@@ -108,12 +124,18 @@ def test_create_date_during():
 
 
 def test_create_bins():
+    """
+    creates 3 bins of random sizes
+    """
     __set_seed()
     parts = __create_bins(20)
     assert parts == PARTS
 
 
 def test_browserfile():
+    """
+    compares processed simulated data with expectated dataframe
+    """
     file_data = __create_zip(__create_browserfile())
     result = process(file_data)
     expected = __reshape_expected()
